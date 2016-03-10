3929d02f5df6de7e4554e7e5b7ba1a0366c41373
yi7 <yi7@njit.edu>
2016-03-10 16:25:01 -0500

    bar chart working, changed pie chart to tip percentage

--- public/js/controllers/TaxiMCtrl.js
+++ public/js/controllers/TaxiMCtrl.js
@@ -4,48 +4,126 @@ var app = angular.module('TaxiMCtrl', []);
 app.controller('TaxiMController', function($scope, $http, TaxiMongo) {
         $scope.tagline = 'Data used to make charts below are stored in mongo';
 
-        d3.select(".descriptionPie").append("p").text("Number of passengers in cab");
-        var canvas = d3.select(".piechart");
         TaxiMongo.getAll().then(function(response) {
             var pCount = {1: 0, 2: 0, 3: 0, 4: 0, 5: 0, 6: 0};
 
+            var yesTip = 0;
+            var noTip = 0;
+
             for( var i = 0; i < response.data.length; i++) {
                 pCount[response.data[i].passenger_count]++;
+                if(response.data[i].tip_amount != 0) {
+                    yesTip++;
+                } else {
+                    noTip++;
+                }
             }
 
-            var data = [pCount[1], pCount[2], pCount[3], pCount[4], pCount[5], pCount[6]];
+            var bardata = [pCount[1], pCount[2], pCount[3], pCount[4], pCount[5], pCount[6]];
+            var piedata = [yesTip, noTip];
 
-            var color = d3.scale.ordinal()
-                .range(["red", "blue", "orange", "yellow", "green", "cyan"]);
-
-            var group = canvas.append("g")
-                .attr("transform", "translate(200, 200)");
-
-            var arc = d3.svg.arc()
-                .innerRadius(0)
-                .outerRadius(200);
-
-            var pie = d3.layout.pie()
-                .value(function(d) { return d });
-
-            var arcs = group.selectAll(".arc")
-                .data(pie(data))
-                .enter()
-                .append("g")
-                .attr("class", "arc");
-
-            arcs.append("path")
-                .attr("d", arc)
-                .attr("fill", function(d) { return color(d.data)});
-
-            arcs.append("text")
-                .attr("transform", function(d) { return "translate(" + arc.centroid(d) + ")"; })
-                .text(function(d, i) { return i + 1; });
+            makePieChart(piedata);
+            makeBarChart(bardata);
         });
 
-        d3.select(".descriptionLiquid").append("p").text("$ of fairs");
+        d3.select(".descriptionLiquid").append("p").text("Test Liquid Gauge");
 
 });
+
+function makePieChart(data) {
+    d3.select(".descriptionPie").append("p").text("Percentage of people who tip");
+    var canvas = d3.select(".piechart");
+
+    var color = d3.scale.ordinal()
+        .range(["#FF7777", "#178BCA", "orange", "yellow", "green", "cyan"]);
+
+    var group = canvas.append("g")
+        .attr("transform", "translate(200, 200)");
+
+    var arc = d3.svg.arc()
+        .innerRadius(0)
+        .outerRadius(200);
+
+    var pie = d3.layout.pie()
+        .value(function(d) { return d });
+
+    var arcs = group.selectAll(".arc")
+        .data(pie(data))
+        .enter()
+        .append("g")
+        .attr("class", "arc");
+
+    arcs.append("path")
+        .attr("d", arc)
+        .attr("fill", function(d) { return color(d.data)});
+
+    arcs.append("text")
+        .attr("transform", function(d) { return "translate(" + arc.centroid(d) + ")"; })
+        .text(function(d, i) {
+            if(i == 0) {
+                return 'Tip';
+            } else {
+                return 'No Tip';
+            }
+        });
+}
+
+function makeBarChart(data) {
+    d3.select(".descriptionBar").append("p").text("Number of passengers in cab");
+
+    var margin = {top: 20, right: 20, bottom: 30, left: 40},
+    width = 800 - margin.left - margin.right,
+    height = 500 - margin.top - margin.bottom;
+
+    var x = d3.scale.ordinal()
+        .rangeRoundBands([0, width], 0.1, 1);
+
+    var y = d3.scale.linear()
+        .range([height, 0]);
+
+    var xAxis = d3.svg.axis()
+        .scale(x)
+        .orient("bottom");
+
+    var yAxis = d3.svg.axis()
+        .scale(y)
+        .orient("left");
+
+    var svg = d3.select(".barchart")
+        .attr("width", width + margin.left + margin.right)
+        .attr("height", height + margin.top + margin.bottom)
+        .append("g")
+        .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+
+
+    x.domain(data.map(function(d, i) { return i + 1; }));
+    y.domain([0, d3.max(data, function(d) { return d; })]);
+
+    svg.append("g")
+        .attr("class", "x axis")
+        .attr("transform", "translate(0," + height + ")")
+        .call(xAxis)
+        .style("text-anchor", "end");
+
+    svg.append("g")
+        .attr("class", "y axis")
+        .call(yAxis)
+        .append("text")
+        .attr("transform", "rotate(-90)")
+        .attr("y", 6)
+        .attr("dy", ".8em")
+        .style("text-anchor", "end")
+        .text("% Frequency");
+
+    svg.selectAll(".bar")
+        .data(data)
+        .enter().append("rect")
+        .attr("class", "bar")
+        .attr("x", function(d, i) { return x(i + 1); })
+        .attr("width", x.rangeBand())
+        .attr("y", function(d) { return y(d); })
+        .attr("height", function(d) { return height - y(d); });
+}
 
 /*app.directive('yokagauge', function() {
     return {
