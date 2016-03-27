b3240f819c28fd42d7975a0a196c5951f91e5e1e
yi7 <yi7@njit.edu>
2016-03-27 16:26:22 -0400

    rectangular area chart interacts with the date selected

--- public/js/controllers/TiaaMCtrl.js
+++ public/js/controllers/TiaaMCtrl.js
@@ -1,36 +1,54 @@
 // public/js/controllers/TaxiCtrl.js
 var app = angular.module('TiaaMCtrl', []);
 
-app.controller('TiaaMController', function($scope, $http, TiaaMongo) {
-    TiaaMongo.query('TRN TRADE_DATE CASH').then(function(response) {
-        var data_list = [];
-        var obj_list = {};
+app.controller('TiaaMController', function($scope, $filter, TiaaMongo) {
 
-        for( var i = 0; i < response.data.length; i++) {
-            if(response.data[i].TRADE_DATE != '7/28/2015') {
-                continue;
+    $scope.change = function(date) {
+        var selected_date = $filter('date')(date, 'M/dd/yyyy');
+
+        TiaaMongo.query('TRN TRADE_DATE CASH').then(function(response) {
+            var data_list = [];
+            var obj_list = {};
+
+            for( var i = 0; i < response.data.length; i++) {
+                if(response.data[i].TRADE_DATE != selected_date) {
+                    continue;
+                }
+
+                if(response.data[i].TRN in obj_list) {
+                    obj_list[response.data[i].TRN] += response.data[i].CASH;
+                } else {
+                    obj_list[response.data[i].TRN] = response.data[i].CASH;
+                }
             }
 
-            if(response.data[i].TRN in obj_list) {
-                obj_list[response.data[i].TRN] += response.data[i].CASH;
-            } else {
-                obj_list[response.data[i].TRN] = response.data[i].CASH;
+            for(var obj in obj_list) {
+                data = {
+                    value: obj_list[obj].toString(),
+                    label: "TRN: " + obj,
+                    valuePrefix: "CASH: "
+                }
+                data_list.push(data);
             }
-        }
 
-        for(var obj in obj_list) {
-            data = {
-                value: obj_list[obj].toString(),
-                label: "TRN: " + obj,
-                valuePrefix: "CASH: "
+            if(data_list.length == 0) {
+                return;
             }
-            data_list.push(data);
-        }
 
-        var config1 = rectangularAreaChartDefaultSettings();
-        config1.expandFromLeft = true;
-        config1.expandFromTop = true;
-        config1.colorsScale = d3.scale.category20b();
-        loadRectangularAreaChart("rectangularareachart1", data_list, config1);
-    });
+            d3.select('svg').remove();
+            d3.select('#rectAreaChart')
+                .append('svg')
+                .attr('id', 'rectangularareachart1')
+                .attr('width', '100%')
+                .attr('height', '250px');
+
+            var config1 = rectangularAreaChartDefaultSettings();
+            config1.expandFromLeft = true;
+            config1.expandFromTop = true;
+            config1.colorsScale = d3.scale.category20b();
+            loadRectangularAreaChart("rectangularareachart1", data_list, config1);
+        });
+    };
+
+
 });
