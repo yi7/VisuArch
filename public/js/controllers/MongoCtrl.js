d565d3fdcbc91077f906860f068f14c546748670
SyntacticX <rmm54@njit.edu>
2016-04-27 17:22:54 -0400

    no message

--- public/js/controllers/MongoCtrl.js
+++ public/js/controllers/MongoCtrl.js
@@ -297,7 +297,7 @@ vis.append('svg:g')
   .attr('transform', 'translate(0,' + (HEIGHT - MARGINS.bottom) + ')')
   .call(xAxis)//;
   .append("text")
-  .attr("x", 200)
+  .attr("x", 6)
   .attr("dx", ".8em")
   .style("text-anchor", "end")
   .text("Date Range 07/21/2015 - 07/30/2015");
