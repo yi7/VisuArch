6f3a9e2ad08befaa3309171dfe9ac3913f474c62
SyntacticX <rmm54@njit.edu>
2016-04-28 13:23:36 -0400

    no message

--- public/js/controllers/MongoCtrl.js
+++ public/js/controllers/MongoCtrl.js
@@ -286,8 +286,8 @@ function lineGraph(data, id) {
     xAxis = d3.svg.axis()
       .scale(xRange)
       .tickSize(1)
-      .tickSubdivide(true),
-      .tickValues(["7/21", "7/22", "7/23", "7/24", "7/25", "7/26", "7/27", "7/28", "7/29", "7/30", ])
+      .tickSubdivide(true)
+      .tickValues(["7/21", "7/22", "7/23", "7/24", "7/25", "7/26", "7/27", "7/28", "7/29", "7/30", ]),
     yAxis = d3.svg.axis()
       .scale(yRange)
       .tickSize(1)
